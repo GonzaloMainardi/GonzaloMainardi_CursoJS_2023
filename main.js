3c9997b96a80632611d1c9cf2398beb03f2097de
GonzaloMainardi <gonzalomainardi21@gmail.com>
2023-04-21 00:35:41 -0300

    arreglo de eventos

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const formaPago = (total) => {
         precioFinal = total * 1.10
         console.log(`METODO DE PAGO: ${formaPago}`);
         console.log(precioFinal);
-        return precioFinal.toFixed(2)
+        return precioFinal.toFixed()
     }
     else {
         alert("ESE NO ES UN METODO DE PAGO VALIDO")
@@ -41,32 +41,65 @@ while (localidad == 0) {
 
   if (localidad === 1 || localidad == 2 || localidad == 3 || localidad == 4) {
     switch (localidad) {
+
       case 1:
         var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: ARANGO \n 2: SIGNO \n 3: PARIS \n 4: PALACIO ALSINA \n 5: NOMADE"));
+        
+        while (evento != 1 && evento != 2 && evento != 3 && evento != 4 && evento && 5) {
+          alert("EVENTO NO ENCONTRADO")
+          evento = 0
+          var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: ARANGO \n 2: SIGNO \n 3: PARIS \n 4: PALACIO ALSINA \n 5: NOMADE"));
+        }
+
         var subtotal = compra(precioEntrada);
         var total = formaPago(subtotal)
         alert(`¡COMPRA EXITOSA, TOTAL: $${total} MUCHAS GRACIAS!`)
+        
         break;
 
       case 2:
         var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: CLUB ARAOZ \n 2: SUCHT \n 3: KIKA CLUB \n 4: JUANA"));
+
+        while (evento != 1 && evento != 2 && evento != 3 && evento != 4) {
+          alert("EVENTO NO ENCONTRADO")
+          evento = 0
+          var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: CLUB ARAOZ \n 2: SUCHT \n 3: KIKA CLUB \n 4: JUANA"));
+        }
+
         var subtotal = compra(precioEntrada);
         var total = formaPago(subtotal)
         alert(`¡COMPRA EXITOSA, TOTAL: $${total} MUCHAS GRACIAS!`)
+
         break;
 
       case 3:
         var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: WAVI \n 2: BLACK JAGGER \n 3: SUBURBIA"));
+
+        while (evento != 1 && evento != 2 && evento != 3) {
+          alert("EVENTO NO ENCONTRADO")
+          evento = 0
+          var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: WAVI \n 2: BLACK JAGGER \n 3: SUBURBIA"));
+        }
+
         var subtotal = compra(precioEntrada);
         var total = formaPago(subtotal)
         alert(`¡COMPRA EXITOSA, TOTAL: $${total} MUCHAS GRACIAS!`)
+
         break;
 
       case 4:
         var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: POINT \n 2: AQUA BARRA \n 3: JAGGER ESPACIO \n 4: SKY"));
+
+        while (evento != 1 && evento != 2 && evento != 3 && evento != 4) {
+          alert("EVENTO NO ENCONTRADO")
+          evento = 0
+          var evento = parseInt(prompt("ELEJI EL EVENTO \n 1: POINT \n 2: AQUA BARRA \n 3: JAGGER ESPACIO \n 4: SKY"));
+        }
+
         var subtotal = compra(precioEntrada);
         var total = formaPago(subtotal)
         alert(`¡COMPRA EXITOSA, TOTAL: $${total} MUCHAS GRACIAS!`)
+        
         break;
 
       default:
