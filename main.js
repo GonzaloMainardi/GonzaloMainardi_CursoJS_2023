d2c07d6f788e0aabc71032e96bc2748284cd0e43
GonzaloMainardi <gonzalomainardi21@gmail.com>
2023-05-30 18:31:41 -0300

    3era pre entrega

--- main.js
+++ main.js
@@ -25,6 +25,9 @@ const productos = [
 ]
 
 const contenedorProductos = document.getElementById("home-productos")
+let btnsComprar = document.querySelectorAll(".btn-comprar")
+
+let carrito = []
 
 function cargarProductos() {
 
@@ -39,6 +42,7 @@ function cargarProductos() {
           <div class="producto-info">
               <h5 class="producto-nombre">${producto.nombre}</h5>
               <h3 class="producto-precio">$${producto.precio}</h3>
+              <a href="#" class="btn-comprar" id="${producto.id}">AÑADIR AL CARRITO</a>
           </div>
       </div>
       `;
@@ -46,173 +50,200 @@ function cargarProductos() {
       contenedorProductos.append(contenedor)
   
   });  
+
+  actualizarBtnsComprar()
   
 }
 
 cargarProductos();
 
-let cantidadProductos = productos.length
+function actualizarBtnsComprar () {
+    btnsComprar = document.querySelectorAll(".btn-comprar")
 
-alert(`DISPONEMOS SOLO ${cantidadProductos} PRODUCTOS PARA OFRECER, QUIERE VER EL CATALOGO?`)
+    btnsComprar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
 
-const catalogoDeProductos = productos.map(({ nombre, precio }) => ({ nombre, precio }));
+function agregarAlCarrito(e) {
+    const btnId = e.currentTarget.id;
+    const productoEnCarrito = productos.find(producto => producto.id === btnId);
 
-alert(`NUESTRO CATALOGO: \n \n - LA ${productos[0].nombre} CUESTA ${productos[0].precio} \n - LA ${productos[1].nombre} CUESTA ${productos[1].precio} \n - LA ${productos[2].nombre} CUESTA ${productos[2].precio}`)
+    if(carrito.some(producto => producto.id === btnId)) {
+        const index = carrito.findIndex(producto => producto.id === btnId);
+        carrito[index].cantidad++;
+    } else {
+        productoEnCarrito.cantidad = 1;
+        carrito.push(productoEnCarrito);
+    }
 
-let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n\n 1) FUNDA DIAMOND \n 2) FUNDA BUMPER CASE \n 3) FUNDA ANTISHOCK \n"));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 
-do {
-    switch (articuloElegido) {
-        case 1:
-            var precioDiamond = productos[0].precio;
-            var fundaColor = ConsultarColor();
-            var fundaCantidad = ConsultarCantidad(productos[0].nombre);
-            var fundaFormaPago = ConsultarFormaPago();
-            var fundaMonto = CalcularMonto(precioDiamond,fundaCantidad);
-            if (fundaFormaPago == 1) {
-                mostrarTicket(fundaCantidad,productos[0].nombre,fundaColor,fundaMonto)
-                articuloElegido = seguirComprando();
-        }
-            else if (fundaFormaPago == 2) {
-                let montoPorCuota = calcularCuota(fundaMonto)
-                mostrarTicket(fundaCantidad,productos[0].nombre,fundaColor,fundaMonto)
-                alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
-                articuloElegido = seguirComprando();
-        }
-            break;
+}
+
+
+// let cantidadProductos = productos.length
+
+// alert(`DISPONEMOS SOLO ${cantidadProductos} PRODUCTOS PARA OFRECER, QUIERE VER EL CATALOGO?`)
+
+// const catalogoDeProductos = productos.map(({ nombre, precio }) => ({ nombre, precio }));
+
+// alert(`NUESTRO CATALOGO: \n \n - LA ${productos[0].nombre} CUESTA ${productos[0].precio} \n - LA ${productos[1].nombre} CUESTA ${productos[1].precio} \n - LA ${productos[2].nombre} CUESTA ${productos[2].precio}`)
+
+// let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n\n 1) FUNDA DIAMOND \n 2) FUNDA BUMPER CASE \n 3) FUNDA ANTISHOCK \n"));
+
+// do {
+//     switch (articuloElegido) {
+//         case 1:
+//             var precioDiamond = productos[0].precio;
+//             var fundaColor = ConsultarColor();
+//             var fundaCantidad = ConsultarCantidad(productos[0].nombre);
+//             var fundaFormaPago = ConsultarFormaPago();
+//             var fundaMonto = CalcularMonto(precioDiamond,fundaCantidad);
+//             if (fundaFormaPago == 1) {
+//                 mostrarTicket(fundaCantidad,productos[0].nombre,fundaColor,fundaMonto)
+//                 articuloElegido = seguirComprando();
+//         }
+//             else if (fundaFormaPago == 2) {
+//                 let montoPorCuota = calcularCuota(fundaMonto)
+//                 mostrarTicket(fundaCantidad,productos[0].nombre,fundaColor,fundaMonto)
+//                 alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
+//                 articuloElegido = seguirComprando();
+//         }
+//             break;
     
-        case 2:
-          let precioBumperCase = productos[1].precio;
-          var fundaColor = ConsultarColor();
-          var fundaCantidad = ConsultarCantidad(productos[1].nombre);
-          var fundaFormaPago = ConsultarFormaPago();
-          var fundaMonto = CalcularMonto(precioBumperCase,fundaCantidad);
-          if (fundaFormaPago == 1) {
-              mostrarTicket(fundaCantidad,productos[1].nombre,fundaColor,fundaMonto)
-              articuloElegido = seguirComprando();
-      }
-          else if (fundaFormaPago == 2) {
-              var montoPorCuota = calcularCuota(fundaMonto)
-              mostrarTicket(fundaCantidad,productos[1].nombre,fundaColor,fundaMonto)
-              alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
-              articuloElegido = seguirComprando();
-      }
-            break;
+//         case 2:
+//           let precioBumperCase = productos[1].precio;
+//           var fundaColor = ConsultarColor();
+//           var fundaCantidad = ConsultarCantidad(productos[1].nombre);
+//           var fundaFormaPago = ConsultarFormaPago();
+//           var fundaMonto = CalcularMonto(precioBumperCase,fundaCantidad);
+//           if (fundaFormaPago == 1) {
+//               mostrarTicket(fundaCantidad,productos[1].nombre,fundaColor,fundaMonto)
+//               articuloElegido = seguirComprando();
+//       }
+//           else if (fundaFormaPago == 2) {
+//               var montoPorCuota = calcularCuota(fundaMonto)
+//               mostrarTicket(fundaCantidad,productos[1].nombre,fundaColor,fundaMonto)
+//               alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
+//               articuloElegido = seguirComprando();
+//       }
+//             break;
     
-        case 3:
-          let precioAntiShock = productos[2].precio;
-          var fundaColor = ConsultarColor();
-          var fundaCantidad = ConsultarCantidad(productos[2].nombre);
-          var fundaFormaPago = ConsultarFormaPago();
-          var fundaMonto = CalcularMonto(precioAntiShock,fundaCantidad);
-          if (fundaFormaPago == 1) {
-              mostrarTicket(fundaCantidad,productos[2].nombre,fundaColor,fundaMonto)
-              articuloElegido = seguirComprando();
-      }
-          else if (fundaFormaPago == 2) {
-              var montoPorCuota = calcularCuota(fundaMonto)
-              mostrarTicket(fundaCantidad,productos[2].nombre,fundaColor,fundaMonto)
-              alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
-              articuloElegido = seguirComprando();
-      }
-                break;  
+//         case 3:
+//           let precioAntiShock = productos[2].precio;
+//           var fundaColor = ConsultarColor();
+//           var fundaCantidad = ConsultarCantidad(productos[2].nombre);
+//           var fundaFormaPago = ConsultarFormaPago();
+//           var fundaMonto = CalcularMonto(precioAntiShock,fundaCantidad);
+//           if (fundaFormaPago == 1) {
+//               mostrarTicket(fundaCantidad,productos[2].nombre,fundaColor,fundaMonto)
+//               articuloElegido = seguirComprando();
+//       }
+//           else if (fundaFormaPago == 2) {
+//               var montoPorCuota = calcularCuota(fundaMonto)
+//               mostrarTicket(fundaCantidad,productos[2].nombre,fundaColor,fundaMonto)
+//               alert(`EL VALOR DE CADA CUOTA A ABONAR ES DE: $${montoPorCuota}`);
+//               articuloElegido = seguirComprando();
+//       }
+//                 break;  
                 
-        default:
+//         default:
     
-            break;
-    }
+//             break;
+//     }
     
-} while (articuloElegido === 1 || articuloElegido == 2 || articuloElegido == 3)
+// } while (articuloElegido === 1 || articuloElegido == 2 || articuloElegido == 3)
 
-function ConsultarColor () {
-    let color = parseInt(prompt("INGRESE EL NUMERO DEL COLOR QUE DESEA COMPRAR: \n\n1) NEGRO \n 2) AZUL \n 3) ROJO \n 4) ROSA \n 5) BLANCO \n"))
-        if (color == 1) {
-            color = "NEGRO";
-        }
-        else if (color == 2) {
-            color = "AZUL";
-        }
-        else if (color == 3) {
-            color = "ROJO";
-        }
-        else if (color == 4) {
-            color = "ROSA";
-        }
-        else if (color == 5) {
-            color = "BLANCO";
-        }
-        else {
-            alert("INGRESO COLOR EQUIVOCADO")
-            ConsultarColor();
-        }
-    return color;
-}
+// function ConsultarColor () {
+//     let color = parseInt(prompt("INGRESE EL NUMERO DEL COLOR QUE DESEA COMPRAR: \n\n1) NEGRO \n 2) AZUL \n 3) ROJO \n 4) ROSA \n 5) BLANCO \n"))
+//         if (color == 1) {
+//             color = "NEGRO";
+//         }
+//         else if (color == 2) {
+//             color = "AZUL";
+//         }
+//         else if (color == 3) {
+//             color = "ROJO";
+//         }
+//         else if (color == 4) {
+//             color = "ROSA";
+//         }
+//         else if (color == 5) {
+//             color = "BLANCO";
+//         }
+//         else {
+//             alert("INGRESO COLOR EQUIVOCADO")
+//             ConsultarColor();
+//         }
+//     return color;
+// }
 
-function ConsultarCantidad (producto) {
-  let cant = prompt(`INGERESE LA CANTIDAD DE ${producto} DESEADA`)
-  return cant
-}
+// function ConsultarCantidad (producto) {
+//   let cant = prompt(`INGERESE LA CANTIDAD DE ${producto} DESEADA`)
+//   return cant
+// }
 
-function ConsultarFormaPago () {
-    let forma = prompt("INGRESE EL NUMERO DE LA FORMA DE PAGO DESEADA: \n\n 1) EFECTIVO O DEBITO \n 2) TARJETA DE CREDITO \n");
-    if (forma == 1) {
-        console.log("USTED DECIDIO ABONAR EN EFECTIVO/DEBITO");
-    }
-    else if (forma == 2){
-        console.log("USTED DECIDIO ABONAR CON TARJETA DE CREDITO ");
-    }
-    else {
-        alert("INGRESO FORMA DE PAGO EQUIVOCADA, INTENTE NUEVAMENTE");
-        ConsultarFormaPago();
-    }
-    return forma;
-}
+// function ConsultarFormaPago () {
+//     let forma = prompt("INGRESE EL NUMERO DE LA FORMA DE PAGO DESEADA: \n\n 1) EFECTIVO O DEBITO \n 2) TARJETA DE CREDITO \n");
+//     if (forma == 1) {
+//         console.log("USTED DECIDIO ABONAR EN EFECTIVO/DEBITO");
+//     }
+//     else if (forma == 2){
+//         console.log("USTED DECIDIO ABONAR CON TARJETA DE CREDITO ");
+//     }
+//     else {
+//         alert("INGRESO FORMA DE PAGO EQUIVOCADA, INTENTE NUEVAMENTE");
+//         ConsultarFormaPago();
+//     }
+//     return forma;
+// }
 
-function CalcularMonto (precio,cantidad) {
-    let montofinal = precio * cantidad;
-    return montofinal;
-}
+// function CalcularMonto (precio,cantidad) {
+//     let montofinal = precio * cantidad;
+//     return montofinal;
+// }
 
-function calcularCuota (monto) {
-    let cuotas = prompt("ELIJA CANTIDAD DE CUOTAS: \n\n 1 CUOTA SIN INTERES \n 3 CUOTAS SIN INTERES \n 6 COUTAS CON 10% DE RECARGO \n");
-    if (cuotas == 1) {
-        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 1 CUOTA SIN INTERES");
-        montoCuota = monto
-    }
-    else if (cuotas == 3) {
-        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 3 CUOTAS SIN INTERES");
-        montoCuota = monto/3
-    }
-    else if (cuotas == 6) {
-        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 6 CUOTAS SIN INTERES CON 10% DE RECARGO");
-        montoCuota = (monto*1.10)/6
-    }
-    else {
-        alert("NO OFRECEMOS ESA CANTIDAD DE CUOTAS, INTENTE NUEVAMENTE");
-        calcularCuota();
+// function calcularCuota (monto) {
+//     let cuotas = prompt("ELIJA CANTIDAD DE CUOTAS: \n\n 1 CUOTA SIN INTERES \n 3 CUOTAS SIN INTERES \n 6 COUTAS CON 10% DE RECARGO \n");
+//     if (cuotas == 1) {
+//         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 1 CUOTA SIN INTERES");
+//         montoCuota = monto
+//     }
+//     else if (cuotas == 3) {
+//         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 3 CUOTAS SIN INTERES");
+//         montoCuota = monto/3
+//     }
+//     else if (cuotas == 6) {
+//         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 6 CUOTAS SIN INTERES CON 10% DE RECARGO");
+//         montoCuota = (monto*1.10)/6
+//     }
+//     else {
+//         alert("NO OFRECEMOS ESA CANTIDAD DE CUOTAS, INTENTE NUEVAMENTE");
+//         calcularCuota();
 
-    }
-    return montoCuota
-}
+//     }
+//     return montoCuota
+// }
 
 
-function seguirComprando() {
+// function seguirComprando() {
 
-    var opcion = confirm("¿DESEA REALIZAR OTRA COMPRA?")
-    if (opcion == true) {
-        let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n 1) FUNDA DIAMOND \n 2) FUNDA BUMPER CASE \n 3) FUNDA ANTISHOCK \n"));
-        return articuloElegido;
-    }
-    else {
-        alert("¡CHOCA ESOS 5! MUCHAS GRACIAS POR ELEJIRNOS");
-    }
+//     var opcion = confirm("¿DESEA REALIZAR OTRA COMPRA?")
+//     if (opcion == true) {
+//         let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n 1) FUNDA DIAMOND \n 2) FUNDA BUMPER CASE \n 3) FUNDA ANTISHOCK \n"));
+//         return articuloElegido;
+//     }
+//     else {
+//         alert("¡CHOCA ESOS 5! MUCHAS GRACIAS POR ELEJIRNOS");
+//     }
 
-}
+// }
 
-function mostrarTicket (cantidad,nombre,color,monto) {
-    console.log("***** TICKET DE COMPRA *****")
-    console.log(`USTED ELIJIO COMPRAR ${cantidad} ${nombre} COLOR: ${color}`);
-    console.log(`EL MONTO FINAL A ABONAR ES DE: $${monto}`);
-    console.log("****************************");
-}
+// function mostrarTicket (cantidad,nombre,color,monto) {
+//     console.log("***** TICKET DE COMPRA *****")
+//     console.log(`USTED ELIJIO COMPRAR ${cantidad} ${nombre} COLOR: ${color}`);
+//     console.log(`EL MONTO FINAL A ABONAR ES DE: $${monto}`);
+//     console.log("****************************");
+// }
 
