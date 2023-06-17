9b4e4582794e098a2237a7dd204bf9d856c8a992
GonzaloMainardi <gonzalomainardi21@gmail.com>
2023-06-17 00:36:43 -0300

    entrega final

--- carrito.js
+++ carrito.js
@@ -9,124 +9,119 @@ const botonesCarrito = document.querySelectorAll("#carrito-vaciar");
 const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-comprar");
 
-function cargarProductosCarrito(){
-    if (carrito && carrito.length > 0) {
+function cargarProductosCarrito() {
+  if (carrito && carrito.length > 0) {
+    carritoVacio.classList.add("disabled");
+    carritoLleno.classList.remove("disabled");
+    carritoAcciones.classList.remove("disabled");
+    carritoComprado.classList.add("disabled");
 
-        carritoVacio.classList.add("disabled");
-        carritoLleno.classList.remove("disabled");
-        carritoAcciones.classList.remove("disabled");
-        carritoComprado.classList.add("disabled");
+    carritoLleno.innerHTML = "";
 
-        carritoLleno.innerHTML = "";
-
-        carrito.forEach(producto => {
-            const contenedorCarrito = document.createElement("div")
-            contenedorCarrito.classList.add("carrito-productos")
-            contenedorCarrito.innerHTML = `
+    carrito.forEach((producto) => {
+      const contenedorCarrito = document.createElement("div");
+      contenedorCarrito.classList.add("carrito-productos");
+      contenedorCarrito.innerHTML = `
             <div class="card" style="width: 18rem;">
-          <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+          <img src="${producto.imagen}" class="card-img-top" alt="${
+        producto.nombre
+      }">
           <div class="producto-info">
               <h5 class="producto-nombre">${producto.nombre}</h5>
               <p class="producto-cantidad">CANTIDAD: ${producto.cantidad}</p>
-              <h3 class="producto-precio">$${producto.precio*producto.cantidad}</h3>
+              <h3 class="producto-precio">$${
+                producto.precio * producto.cantidad
+              }</h3>
           </div>
       </div>
-            `
-            carritoLleno.append(contenedorCarrito);
-        });
+            `;
+      carritoLleno.append(contenedorCarrito);
+    });
+  } else {
+    carritoVacio.classList.remove("disabled");
+    carritoLleno.classList.add("disabled");
+    carritoAcciones.classList.add("disabled");
+    carritoComprado.classList.add("disabled");
+  }
 
-    }
-
-    else {
-        carritoVacio.classList.remove("disabled");
-        carritoLleno.classList.add("disabled");
-        carritoAcciones.classList.add("disabled");
-        carritoComprado.classList.add("disabled");
-    }
-
-    actualizarBtnsCarrito();
-    actualizarTotal();
+  actualizarBtnsCarrito();
+  actualizarTotal();
 }
 
 cargarProductosCarrito();
 
+function actualizarBtnsCarrito() {
+  btnsCarrito = document.querySelectorAll(".carrito-vaciar");
 
-function actualizarBtnsCarrito () {
-    btnsCarrito = document.querySelectorAll(".carrito-vaciar")
-
-    btnsCarrito.forEach(boton => {
-        boton.addEventListener("click", eliminarCarrito);
-    });
+  btnsCarrito.forEach((boton) => {
+    boton.addEventListener("click", eliminarCarrito);
+  });
 }
 
+function eliminarCarrito() {
+  Swal.fire({
+    title: "DESEA ELIMINAR EL CARRITO?",
+    text: "NO PUEDE VOLVER ATRAS!",
+    icon: "warning",
+    showDenyButton: true,
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "CONFIRMAR",
+    denyButtonText: `CANCELAR`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "CARRITO ELIMINADO",
+        showConfirmButton: false,
+        timer: 1200,
+      });
 
-function eliminarCarrito () {
-    Swal.fire({
-        title: 'DESEA ELIMINAR EL CARRITO?',
-        text: "NO PUEDE VOLVER ATRAS!",
-        icon: 'warning',
-        showDenyButton: true,
-        confirmButtonColor: '#008000',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'CONFIRMAR',
-        denyButtonText: `CANCELAR`,
-      }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'CARRITO ELIMINADO',
-                showConfirmButton: false,
-                timer: 1200,
-              })
-
-        carrito.length = 0;
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        cargarProductosCarrito()
-
-        }
-
-      })
+      carrito.length = 0;
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      cargarProductosCarrito();
+    }
+  });
 }
 
 function actualizarTotal() {
-    const totalCalculado = carrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    total.innerText = `$${totalCalculado}`;
+  const totalCalculado = carrito.reduce(
+    (acc, producto) => acc + producto.precio * producto.cantidad,
+    0
+  );
+  total.innerText = `$${totalCalculado}`;
 }
 
-botonComprar.addEventListener("click",comprarCarrito)
+botonComprar.addEventListener("click", comprarCarrito);
 
-function comprarCarrito () {
+function comprarCarrito() {
+  Swal.fire({
+    title: "ESTA SEGURO DE REALIZAR LA COMPRA?",
+    icon: "warning",
+    showDenyButton: true,
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "CONFIRMAR",
+    denyButtonText: `CANCELAR`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "COMPRA EXITOSA",
+        showConfirmButton: false,
+        timer: 1200,
+      });
 
-    Swal.fire({
-        title: 'ESTA SEGURO DE REALIZAR LA COMPRA?',
-        icon: 'warning',
-        showDenyButton: true,
-        confirmButtonColor: '#008000',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'CONFIRMAR',
-        denyButtonText: `CANCELAR`,
-      }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'COMPRA EXITOSA',
-                showConfirmButton: false,
-                timer: 1200,
-              })
+      carrito.length = 0;
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      cargarProductosCarrito();
+    }
+  });
 
-        carrito.length = 0;
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        cargarProductosCarrito()
-
-        }
-
-      })
-
-    carritoVacio.classList.add("disabled");
-    carritoLleno.classList.add("disabled");
-    carritoAcciones.classList.add("disabled");
-    carritoComprado.classList.remove("disabled");
-
+  carritoVacio.classList.add("disabled");
+  carritoLleno.classList.add("disabled");
+  carritoAcciones.classList.add("disabled");
+  carritoComprado.classList.remove("disabled");
 }
--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const traerDatos = async () => {
-    try{
+  try {
     const response = await fetch("./data.json");
     productos = await response.json();
     contenedorProductos.innerHTML = "";
 
     productos.forEach((producto) => {
-    
-        const contenedor = document.createElement("div")
-        contenedor.innerHTML = `
+      const contenedor = document.createElement("div");
+      contenedor.innerHTML = `
         <div class="card" style="width: 18rem;">
             <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
             <div class="producto-info">
@@ -17,56 +16,51 @@ const traerDatos = async () => {
             </div>
         </div>
         `;
-    
-        contenedorProductos.append(contenedor)
-    
-    });  
-  
-    actualizarBtnsComprar()
 
-    }
-
-    catch (error){
-        alert("error")
-    }
-}
-
-traerDatos()
-
-const contenedorProductos = document.getElementById("home-productos")
-let btnsComprar = document.querySelectorAll(".btn-comprar")
-
-let carrito = []
-
-function actualizarBtnsComprar () {
-    btnsComprar = document.querySelectorAll(".btn-comprar")
-
-    btnsComprar.forEach(boton => {
-        boton.addEventListener("click", agregarAlCarrito);
+      contenedorProductos.append(contenedor);
     });
+
+    actualizarBtnsComprar();
+  } catch (error) {
+    alert("error");
+  }
+};
+
+traerDatos();
+
+const contenedorProductos = document.getElementById("home-productos");
+let btnsComprar = document.querySelectorAll(".btn-comprar");
+
+let carrito = [];
+
+function actualizarBtnsComprar() {
+  btnsComprar = document.querySelectorAll(".btn-comprar");
+
+  btnsComprar.forEach((boton) => {
+    boton.addEventListener("click", agregarAlCarrito);
+  });
 }
 
 function agregarAlCarrito(e) {
-    Toastify({
-        text: "PRODUCTO AGREGADO",
-        duration: 1000,
-        gravity: "top", 
-        position: "center",
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-      }).showToast();
-    const btnId = e.currentTarget.id;
-    const productoEnCarrito = productos.find(producto => producto.id === btnId);
+  Toastify({
+    text: "PRODUCTO AGREGADO",
+    duration: 1000,
+    gravity: "top",
+    position: "center",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+  const btnId = e.currentTarget.id;
+  const productoEnCarrito = productos.find((producto) => producto.id === btnId);
 
-    if(carrito.some(producto => producto.id === btnId)) {
-        const index = carrito.findIndex(producto => producto.id === btnId);
-        carrito[index].cantidad++;
-    } else {
-        productoEnCarrito.cantidad = 1;
-        carrito.push(productoEnCarrito);
-    }
+  if (carrito.some((producto) => producto.id === btnId)) {
+    const index = carrito.findIndex((producto) => producto.id === btnId);
+    carrito[index].cantidad++;
+  } else {
+    productoEnCarrito.cantidad = 1;
+    carrito.push(productoEnCarrito);
+  }
 
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
